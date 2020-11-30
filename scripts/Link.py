6bba1b7d59ccfee6cbc0971d3da51eac9f8d976a
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2020-11-30 16:28:57 +0530

    Initial commit

---
+++ scripts/Link.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+os.chdir("..\src")
+Content = os.listdir()
+
+Empty = ""
+Files = []
+for Filenames in Content:
+    if Filenames.endswith(".cpp") and not Filenames.startswith("Glass.cpp"):
+        Files.append(Filenames)
+
+for Iterations in Files:
+    Empty += f" {Iterations}"
+
+os.system(f"g++ Glass.cpp{Empty}");
+sys.exit()
