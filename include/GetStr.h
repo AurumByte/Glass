961dafe2232d5a199780d307fea9b93577f71d43
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2020-12-07 08:10:13 +0530

    Initial commit

--- include/GetStr.h
+++ include/GetStr.h
@@ -4,5 +4,6 @@
 
 bool startswith(const std::string& StartString, const std::string& SubStart);
 bool endswith(const std::string& EndString, const std::string& SubEnd);
-bool getString(const std::string& StartStr, const std::string& s1, const std::string& EndStr, const std::string& s2);
-std::string replace(const std::string& MainString, const std::string& ToRepl, const std::string& WithRepl);
+bool getString(const std::string& StartStr, const std::string& s1, const std::string& s2);
+std::string replace(const std::string MainString, const std::string& ToRepl, const std::string& WithRepl);
+std::string join(std::string& JoinString, const std::string& JoinWith);
