0fa620a08a1672fad17d02f7fb3a480f5cfc341b
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-01-11 20:04:58 +0530

    Made Error function and Count variable global

--- include/Glass.h
+++ include/Glass.h
@@ -11,6 +11,12 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "../include/GetStr.h"
+#include "../include/System.h"
+#include "../include/Generic.h"
+#include "../include/PostThread.h"
+#include "../include/Collections.h"
+
 extern std::map<std::string, std::string> variables;
 extern std::map<std::string, std::string> VarTypes;
 
@@ -20,3 +26,7 @@ extern int Postpart;
 extern int FileIOpart;
 extern int Genericpart;
 extern int Collectionpart;
+
+extern int Count;
+
+void Error(const std::string lines, const std::string num);
