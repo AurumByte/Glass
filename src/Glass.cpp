60dd458c88da20ac2285bb40d0a40c7103bcc28e
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-01-18 21:21:05 +0530

    Add help, version and multiline comment system

--- src/Glass.cpp
+++ src/Glass.cpp
@@ -38,9 +38,12 @@ int main(int argc, char** argv)
     // Initializing Glass
     DirName = argv[1];
     if (endswith(DirName, ".glass")) in.open(DirName);
+    else if (DirName == "-version") cout << "Glass 2021 [Alpha 0.7]" << endl;
+    else if (DirName == "-help") cout << "-version :     Shows the version of Glass" << endl;
     else
     {
     	system("color 04");
+        cout << "Type -help for more info." << endl;
     	cout << "Cannot read \"" << DirName << "\"" << endl;
     	exit(0);
     }
@@ -54,6 +57,7 @@ int main(int argc, char** argv)
     Genericpart = 0;
     Collectionpart = 0;
 
+    int Comments = 0;
     // Main langauge
     while (in)
     {
@@ -62,39 +66,41 @@ int main(int argc, char** argv)
         // These are not part of any Package.
         if (Line == "" || Line == " " || startswith(Line, "  ")) continue;
         else if (startswith(Line, "//")) continue;
+        else if (startswith(Line, "/*") && Comments == 0) Comments = 1;
+        else if (endswith(Line, "*/") && Comments == 1) Comments = 0;
 
         // These are the Package caller.
-        else if (Line == "using System;") Syspart = 1;
-        else if (Line == "using FileIO;") FileIOpart = 1;
-        else if (Line == "using Generic;") Genericpart = 1;
-        else if (Line == "using PostThread;") Postpart = 1;
-        else if (Line == "using Webbrowser;") Webbrowser = 1;
-        else if (Line == "using Cgr.Graphics;") Cgrpart = 1;
-        else if (Line == "using Collections.DataTypes;") Collectionpart = 1;
+        else if (Line == "using System;" && Comments == 0) Syspart = 1;
+        else if (Line == "using FileIO;" && Comments == 0) FileIOpart = 1;
+        else if (Line == "using Generic;" && Comments == 0) Genericpart = 1;
+        else if (Line == "using PostThread;" && Comments == 0) Postpart = 1;
+        else if (Line == "using Webbrowser;" && Comments == 0) Webbrowser = 1;
+        else if (Line == "using Cgr.Graphics;" && Comments == 0) Cgrpart = 1;
+        else if (Line == "using Collections.DataTypes;" && Comments == 0) Collectionpart = 1;
 
         // These are the part of the System Package.
-        else if (Line == "System.Exit();" && Syspart == 1) exit(0);
-        else if (Line == "Console.Clear();" && Syspart == 1) ConsoleClear();
-        else if (getString(Line, "Console.CMD(", ");") && Syspart == 1) ConsoleCMD(Line);
-        else if (getString(Line, "Console.Title(", ");") && Syspart == 1) ConsoleTitle(Line);
-        else if (getString(Line, "Console.Color(", ");") && Syspart == 1) ConsoleColor(Line);
-        else if (getString(Line, "Shout(", ");") && Syspart == 1) ConsoleShout(Line);
-        else if (getString(Line, "Shoutln(", ");") && Syspart == 1) ConsoleShoutln(Line);
-        else if (getString(Line, "Get(", ");") && Syspart == 1)
+        else if (Line == "System.Exit();" && Syspart == 1 && Comments == 0) exit(0);
+        else if (Line == "Console.Clear();" && Syspart == 1 && Comments == 0) ConsoleClear();
+        else if (getString(Line, "Console.CMD(", ");") && Syspart == 1 && Comments == 0) ConsoleCMD(Line);
+        else if (getString(Line, "Console.Title(", ");") && Syspart == 1 && Comments == 0) ConsoleTitle(Line);
+        else if (getString(Line, "Console.Color(", ");") && Syspart == 1 && Comments == 0) ConsoleColor(Line);
+        else if (getString(Line, "Shout(", ");") && Syspart == 1 && Comments == 0) ConsoleShout(Line);
+        else if (getString(Line, "Shoutln(", ");") && Syspart == 1 && Comments == 0) ConsoleShoutln(Line);
+        else if (getString(Line, "Get(", ");") && Syspart == 1 && Comments == 0)
         {
             string Input = ConsoleGet(Line);
             getline(cin, Input);
         }
 
         // These are the part of PostThread Package.
-        else if (getString(Line, "Time.Sleep(", ");") && Postpart == 1) PostThreadSleep(Line);
+        else if (getString(Line, "Time.Sleep(", ");") && Postpart == 1 && Comments == 0) PostThreadSleep(Line);
 
         // These are the part of the Generic Package.
-        else if (getString(Line, "Random.rand(", ");") && Genericpart == 1) RandomRange(Line);
-        else if (getString(Line, "Random.Uniform(", ");") && Genericpart == 1) RandomUniform(Line);
+        else if (getString(Line, "Random.rand(", ");") && Genericpart == 1 && Comments == 0) RandomRange(Line);
+        else if (getString(Line, "Random.Uniform(", ");") && Genericpart == 1 && Comments == 0) RandomUniform(Line);
 
         // These are the part of Collections Package.
-        else if (getString(Line, "string ", ";") && Collectionpart == 1)
+        else if (getString(Line, "string ", ";") && Collectionpart == 1 && Comments == 0)
         {
             string Value = DataTypeStr(Line);
             string Type = "type<string>";
@@ -106,7 +112,7 @@ int main(int argc, char** argv)
             VarTypes[Title] = Type;
         }
 
-        else if (getString(Line, "int ", ";") && Collectionpart == 1)
+        else if (getString(Line, "int ", ";") && Collectionpart == 1 && Comments == 0)
         {
             string Value = DataTypeInt(Line);
             string Type = "type<int>";
@@ -118,7 +124,7 @@ int main(int argc, char** argv)
             VarTypes[Title] = Type;
         }
 
-        else if (getString(Line, "float ", ";") && Collectionpart == 1)
+        else if (getString(Line, "float ", ";") && Collectionpart == 1 && Comments == 0)
         {
             string Value = DataTypeFloat(Line);
             string Type = "type<float>";
@@ -130,7 +136,7 @@ int main(int argc, char** argv)
             VarTypes[Title] = Type;
         }
 
-        else if (getString(Line, "bool ", ";") && Collectionpart == 1)
+        else if (getString(Line, "bool ", ";") && Collectionpart == 1 && Comments == 0)
         {
             string Value = DataTypeBool(Line);
             string Type = "type<boolean>";
@@ -143,7 +149,11 @@ int main(int argc, char** argv)
         }
 
         // This will Catch an Exceptional error.
-        else Error(Line, to_string(Count));
+        else
+        {
+            if (Comments == 0) Error(Line, to_string(Count));
+            else continue;
+        }
     }
     in.close();
     return 0;
